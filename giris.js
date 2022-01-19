5c472e42553c7e0c4ef16389664c14419607c730
mehmet-han <mhatunoglu8034@gmail.com>
2022-01-19 17:12:15 +0300

    İlk deneme

---
+++ giris.js
@@ -0,0 +1,10 @@
+const a=0;
+
+console.log(a);
+// yeni bir repo 
+// repoyu bilgisayara colonlamak
+// dosyada değilik yapmak
+// bunu lokalle kaydedip
+// github push la
+// github üye 
+//git pul 
